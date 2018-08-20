35de7c16a7995ef445d06a5eabbe322d05bc722f
sergik2907 <38475176+sergik2907@users.noreply.github.com>
2018-08-20 20:51:49 +0300

    #2: Added new employees

--- demo/src/main/java/com/example/demo/DatabaseLoader.java
+++ demo/src/main/java/com/example/demo/DatabaseLoader.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-
+@Component
 public class DatabaseLoader implements CommandLineRunner {
 
     private final EmployeeRepository repository;
@@ -17,5 +17,10 @@ public class DatabaseLoader implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
         this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+	this.repository.save(new Employee("Bilbo", "Baggins", "burglar"));
+	this.repository.save(new Employee("Gandalf", "the Grey", "wizard"));
+	this.repository.save(new Employee("Samwise", "Gamgee", "gardener"));
+	this.repository.save(new Employee("Meriadoc", "Brandybuck", "pony rider"));
+	this.repository.save(new Employee("Peregrin", "Took", "pipe smoker"));
     }
 }
